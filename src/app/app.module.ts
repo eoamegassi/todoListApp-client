719ff94ab1a252239df9e47d17ffcacf9728efd8
Elisee Othniel AMEGASSI <eliseothniel@gmail.com>
2019-05-15 06:21:18 +0000

    Add components, service and model according to the previous ToDo app project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TasksAddComponent } from './tasks/tasks-add/tasks-add.component';
+import { TasksListComponent } from './tasks/tasks-list/tasks-list.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TasksAddComponent,
+    TasksListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
